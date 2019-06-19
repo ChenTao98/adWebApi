fdb3c36aaaa6ba1fb57ea070446a336713fe3de8
zhaoyangyingmu <16302010029@fudan.edu.cn>
2019-06-19 21:08:02 +0800

    笔记部分做完了。

--- src/main/java/com/adweb/adweb/utils/errorCode/ErrorCode.java
+++ src/main/java/com/adweb/adweb/utils/errorCode/ErrorCode.java
@@ -9,8 +9,12 @@ public enum ErrorCode {
     // 用户相关
         // 收藏和笔记部分
         ADD_COLLECTION_FAILED(2001, "添加收藏失败！"),
-        DELETE_COLLECTION_FAILED(2002, "取消收藏失败！")
+        DELETE_COLLECTION_FAILED(2002, "取消收藏失败！"),
+        ADD_NOTE_FAILED(2003, "添加笔记失败！"),
+        MODIFY_NOTE_FAILED(2004, "修改笔记失败！"),
+        DELETE_NOTE_FAILED(2005, "删除笔记失败！");
     ;
+
 
 
     private int errorCode;
