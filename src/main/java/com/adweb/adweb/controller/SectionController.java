948ecd5f295d536f8e9cf4335f6a521c2e32d6f1
radarcly <16302010001@fudan.edu.cn>
2019-06-20 21:38:36 +0800

    10.完成获取小节测试

--- src/main/java/com/adweb/adweb/controller/SectionController.java
+++ src/main/java/com/adweb/adweb/controller/SectionController.java
@@ -2,6 +2,7 @@ package com.adweb.adweb.controller;
 
 import com.adweb.adweb.JsonUtil.JsonUtils;
 import com.adweb.adweb.JsonUtil.MyJson;
+import com.adweb.adweb.service.HomeworkService;
 import com.adweb.adweb.service.KnowledgeService;
 import com.alibaba.fastjson.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,9 +10,11 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("section")
-public class KnowledgeController {
+public class SectionController {
     @Autowired
     private KnowledgeService knowledgeService;
+    @Autowired
+    private HomeworkService homeworkService;
     //9获得小节所有知识点
     @GetMapping(value = "{section_id}/knowledge",produces = "application/json;utf-8")
     public String course_selection(@PathVariable() int section_id){
@@ -21,6 +24,14 @@ public class KnowledgeController {
 
         return jsonObject.toString();
     }
+    //10获得小节的作业题
+    @GetMapping(value = "{section_id}/question",produces = "application/json;utf-8")
+    public String course_selection(@PathVariable() int section_id,@RequestParam("openId") String open_id){
+        JSONObject jsonObject=new MyJson();
+        JsonUtils.setSuccess(jsonObject);
+        jsonObject.put("dataList",homeworkService.getHomeworkBySectionID(section_id,open_id));
 
+        return jsonObject.toString();
+    }
 
 }
