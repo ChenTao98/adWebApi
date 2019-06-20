29e93fef1d58138088e83d1f52995b97139b5952
radarcly <16302010001@fudan.edu.cn>
2019-06-20 14:07:10 +0800

    更新获取学生基本信息，以header形式

--- src/main/java/com/adweb/adweb/controller/StudentController.java
+++ src/main/java/com/adweb/adweb/controller/StudentController.java
@@ -3,14 +3,12 @@ package com.adweb.adweb.controller;
 import com.adweb.adweb.JsonUtil.JsonUtils;
 import com.adweb.adweb.JsonUtil.MyJson;
 
+import com.adweb.adweb.entity.Student;
 import com.adweb.adweb.service.CourseService;
 import com.adweb.adweb.service.StudentService;
 import com.alibaba.fastjson.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("student")
@@ -18,12 +16,12 @@ public class StudentController {
     @Autowired
     private StudentService studentService;
 
-    @GetMapping(value = "credit/{studentID}",produces = "application/json;UT8-8")
-    public String getMyCredit(@PathVariable() String studentID){
+    @GetMapping(value = "",produces = "application/json;UT8-8")
+    public String getMyCredit(@RequestHeader(name="openID") String studentID){
         JSONObject jsonObject=new MyJson();
         JsonUtils.setSuccess(jsonObject);
-        Integer credit=studentService.getMyCredit(studentID);
-        jsonObject.put("credit",credit);
+        Student stu=studentService.getStuInfo(studentID);
+        jsonObject.put("data",stu);
         return jsonObject.toString();
     }
     @GetMapping(value = "course/{studentID}",produces = "application/json;UT8-8")
@@ -33,5 +31,4 @@ public class StudentController {
         jsonObject.put("dataList",studentService.getMyCourse(studentID));
         return jsonObject.toString();
     }
-
 }
